596b9387da5c3d68bcfd6da53a44b2e781903fb7
jjhsnail0822 <jjhsnail0822@gmail.com>
2023-11-04 16:06:56 +0900

    Fix db_crawl.py

--- db_crawl.py
+++ db_crawl.py
@@ -3,11 +3,12 @@
 #각각 책 제목, 각 책이 여러권으로 이루어져있는 경우 해당 권의 제목, 챕터 이름, 페이지를 나타내는 숫자이다.
 #이를 book_name, book_number, chap_number, page_num 4개의 변수로 분할하여 웹페이지를 탐색한다.
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 import os
 import json
 
-driver = webdriver.Edge()
+driver = webdriver.Chrome()
 book_name = '1370'
 book_number = '0020'
 chap_number = '000'
@@ -23,10 +24,10 @@ while (int(book_number)<=280):
     try:
         url = f"https://db.itkc.or.kr/dir/item?itemId=BT#dir/node?grpId=&itemId=BT&gubun=book&depth=4&cate1=H&cate2=&dataGubun=%EC%B5%9C%EC%A2%85%EC%A0%95%EB%B3%B4&dataId=ITKC_BT_{book_name}A_{book_number}_{chap_number}_{page_num}&viewSync=OT"
         driver.get(url)
-        time.sleep(1)
+        time.sleep(0.1)
         # XPath를 사용하여 원하는 데이터 추출
-        data_kor = driver.find_element_by_xpath('/html/body/div[2]/section[2]/section[2]/section/div[2]/div[1]/div/div[3]').text
-        data_chn = driver.find_element_by_xpath('/html/body/div[2]/section[2]/section[2]/section/div[2]/div[2]/div/div/div[3]').text
+        data_kor = driver.find_element(By.XPATH, '/html/body/div[2]/section[2]/section[2]/section/div[2]/div[1]/div/div[3]').text
+        data_chn = driver.find_element(By.XPATH, '/html/body/div[2]/section[2]/section[2]/section/div[2]/div[2]/div/div/div[3]').text
         kor_filename = f"kor_output/kor_{book_name}_{book_number}_{chap_number}_{page_num}.json"
         chn_filename = f"chn_output/chn_{book_name}_{book_number}_{chap_number}_{page_num}.json"
 
