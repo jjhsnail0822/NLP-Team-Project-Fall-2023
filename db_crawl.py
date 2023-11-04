6c346182dd47d738b2b0fb09779998058739027e
유준상 <mslee020807@gmail.com>
2023-11-04 12:52:06 +0900

    Update db_crawl.py

--- db_crawl.py
+++ db_crawl.py
@@ -4,40 +4,48 @@
 #이를 book_name, book_number, chap_number, page_num 4개의 변수로 분할하여 웹페이지를 탐색한다.
 from selenium import webdriver
 import time
-
+import os
+import json
 
 driver = webdriver.Edge()
 book_name = '1370'
-book_number = '0160'
+book_number = '0020'
 chap_number = '000'
 page_num = '0010'
 last_fail = False
-while (int(book_number)<=190):
+
+if not os.path.exists("kor_output"):
+    os.makedirs("kor_output")
+if not os.path.exists("chn_output"):
+    os.makedirs("chn_output")
+while (int(book_number)<=280):
     str(book_number).zfill(4)
     try:
         url = f"https://db.itkc.or.kr/dir/item?itemId=BT#dir/node?grpId=&itemId=BT&gubun=book&depth=4&cate1=H&cate2=&dataGubun=%EC%B5%9C%EC%A2%85%EC%A0%95%EB%B3%B4&dataId=ITKC_BT_{book_name}A_{book_number}_{chap_number}_{page_num}&viewSync=OT"
         driver.get(url)
+        time.sleep(1)
         # XPath를 사용하여 원하는 데이터 추출
         data_kor = driver.find_element_by_xpath('/html/body/div[2]/section[2]/section[2]/section/div[2]/div[1]/div/div[3]').text
         data_chn = driver.find_element_by_xpath('/html/body/div[2]/section[2]/section[2]/section/div[2]/div[2]/div/div/div[3]').text
+        kor_filename = f"kor_output/kor_{book_name}_{book_number}_{chap_number}_{page_num}.json"
+        chn_filename = f"chn_output/chn_{book_name}_{book_number}_{chap_number}_{page_num}.json"
 
-        # 데이터를 output.txt 파일에 저장
-        with open('output_kor.txt', 'a', encoding='utf-8') as output_file_kor:
-            output_file_kor.write(data_kor + '\n')
-        with open('output_chn.txt', 'a', encoding='utf-8') as output_file_chn:
-            output_file_chn.write(data_chn + '\n')
+        with open(kor_filename, 'w', encoding='utf-8') as output_file_kor:
+            json.dump({"data": data_kor}, output_file_kor, ensure_ascii=False)
 
-        print("데이터가 output.txt 파일에 저장되었습니다.")
+        with open(chn_filename, 'w', encoding='utf-8') as output_file_chn:
+            json.dump({"data": data_chn}, output_file_chn, ensure_ascii=False)
+
+        print(f"데이터가 {kor_filename} 및 {chn_filename} 파일에 저장되었습니다.")
         last_fail = False
         page_num = str(int(page_num) + 10).zfill(4)
-        time.sleep(1)
+
     except Exception as e:
         if last_fail == False:
             # page_num, chap_number 값을 조정하여 재시도
             page_num = '0010'
             chap_number = str(int(chap_number) + 10).zfill(3)
             last_fail = True
-            time.sleep(2)
         else:
             # page_num, chap_number, book_number 값을 모두 조정하여 재시도
             page_num = '0010'
